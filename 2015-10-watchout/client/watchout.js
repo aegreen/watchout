0bd6b360934097bdb49d34fe76ac5c876b2d97ed
Alice Green <green.alice.e@gmail.com>
2015-10-16 18:25:16 -0500

    Created gameboard

---
+++ 2015-10-watchout/client/watchout.js
@@ -0,0 +1,9 @@
+// start slingin' some d3 here.
+var width = 750;
+
+var height = 500;
+
+var svg = d3.select('body').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .style('background-color', 'black');
