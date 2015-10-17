50c2ebc66e9a5af20c0cdb1e1a0c628fc92232c0
Alice Green <green.alice.e@gmail.com>
2015-10-16 19:37:02 -0500

    Working on a function to move the asteroids

--- 2015-10-watchout/client/watchout.js
+++ 2015-10-watchout/client/watchout.js
@@ -1,7 +1,7 @@
 // constants for the game environment
 var width = 750;
 var height = 500;
-var nAsteroids = 15;
+var nAsteroids = 30;
 var padding = 10;
 
 
@@ -13,15 +13,21 @@ var gameboard = d3.select('body').append('svg')
     .style('background-color', 'black')
   .append('g');
 
-var createAsteroid = function(n) {
+var createAsteroids = function(n) {
   for (var i = 1; i <= n; i++) {
+    var x = Math.random() * (width - 25);
+    var y = Math.random() * (height - 25);
     d3.select('.gameboard').append('svg').append('image')
       .attr('width', '25px')
       .attr('height', '25px')
-      .attr('x', 25 * i)
-      .attr('y', 25)
+      .attr('x', x)
+      .attr('y', y)
+      .attr('class', 'asteroid')
       .attr('xlink:href', 'asteroid.png');
   }
 };
 
-createAsteroid(nAsteroids);
+createAsteroids(nAsteroids);
+
+
+setInterval(d3.selectAll('.asteroid').attr('x', Math.random() * (width)), 1500)
