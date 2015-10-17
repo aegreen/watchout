95d06a3a896285eaa5a0c0e102b09c1327154d89
Alice Green <green.alice.e@gmail.com>
2015-10-16 19:14:19 -0500

    Added asteroids to the page

--- 2015-10-watchout/client/watchout.js
+++ 2015-10-watchout/client/watchout.js
@@ -1,9 +1,27 @@
-// start slingin' some d3 here.
+// constants for the game environment
 var width = 750;
-
 var height = 500;
+var nAsteroids = 15;
+var padding = 10;
 
-var svg = d3.select('body').append('svg')
+
+var gameboard = d3.select('body').append('svg')
+    .attr('class', 'gameboard')
     .attr('width', width)
     .attr('height', height)
-    .style('background-color', 'black');
+    .style('padding', padding)
+    .style('background-color', 'black')
+  .append('g');
+
+var createAsteroid = function(n) {
+  for (var i = 1; i <= n; i++) {
+    d3.select('.gameboard').append('svg').append('image')
+      .attr('width', '25px')
+      .attr('height', '25px')
+      .attr('x', 25 * i)
+      .attr('y', 25)
+      .attr('xlink:href', 'asteroid.png');
+  }
+};
+
+createAsteroid(nAsteroids);
