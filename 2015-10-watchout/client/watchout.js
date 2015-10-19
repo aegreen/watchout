3e77082148ad9611376883da2e00af88a1c739ec
Alice Green <green.alice.e@gmail.com>
2015-10-19 13:43:03 -0500

    Changes reflecting the solution lecture

--- 2015-10-watchout/client/watchout.js
+++ 2015-10-watchout/client/watchout.js
@@ -1,33 +1,105 @@
-// constants for the game environment
-var width = 750;
-var height = 500;
-var nAsteroids = 30;
-var padding = 10;
-
-
-var gameboard = d3.select('body').append('svg')
-    .attr('class', 'gameboard')
-    .attr('width', width)
-    .attr('height', height)
-    .style('padding', padding)
-    .style('background-color', 'black')
-  .append('g');
-
-var createAsteroids = function(n) {
-  for (var i = 1; i <= n; i++) {
-    var x = Math.random() * (width - 25);
-    var y = Math.random() * (height - 25);
-    d3.select('.gameboard').append('svg').append('image')
-      .attr('width', '25px')
-      .attr('height', '25px')
-      .attr('x', x)
-      .attr('y', y)
-      .attr('class', 'asteroid')
-      .attr('xlink:href', 'asteroid.png');
-  }
+///////////////////////////////////////////////////////////
+// Game Settings
+var settings = { 
+  w: 750,
+  h: 414,
+  r: 10,
+  n: 30,
+  duration: 2000
 };
 
-createAsteroids(nAsteroids);
+var mouse = { x: settings.w/2, y: settings.h/2 };
+var score = 0, highScore = 0, collisionCount = 0;
 
+var pixelize = function(number) { return number + 'px'; }
 
-setInterval(d3.selectAll('.asteroid').attr('x', Math.random() * (width)), 1500)
+var rand = function(n) { return Math.floor(Math.random() * n); }
+var randX = function() { return pixelize(rand(settings.w-settings.r*2)); }
+var randY = function() { return pixelize(rand(settings.h-settings.r*2)); }
+
+var updateScore = function() {
+  d3.select('.scoreboard .current span').text(score);
+  d3.select('.scoreboard .highscore span').text(highScore);
+  d3.select('.scoreboard .collisions span').text(collisionCount);
+}
+
+///////////////////////////////////////////////////////////
+
+var board = d3.select('body').style({
+    width: pixelize(settings.w),
+    height: pixelize(settings.h)
+});
+
+d3.select('.mouse').style({
+  top: pixelize(mouse.y),
+  left: pixelize(mouse.x),
+  width: pixelize(settings.r*2),
+  height: pixelize(settings.r*2),
+  'border-radius': pixelize(settings.r*2)
+});
+
+var asteroids = board.selectAll('.asteroid')
+      .data(d3.range(settings.n))
+      .enter().append('div')
+        .attr('class', 'asteroid')
+        .style({
+          top: randY,
+          left: randX,
+          width: pixelize(settings.r*2),
+          height: pixelize(settings.r*2)
+        });
+
+board.on('mousemove', function() {
+  var loc = d3.mouse(this);
+  mouse = { x: loc[0], y: loc[1] };
+  d3.select('.mouse').style({
+    top: pixelize( mouse.y - settings.r ),
+    left: pixelize( mouse.x - settings.r)
+  })
+})
+
+var move = function(element) {
+  element.transition().duration(settings.duration).style({
+    top: randY,
+    left: randX,
+  }).each('end', function() {
+    move(d3.select(this))
+  })
+};
+move(asteroids);
+
+var scoreTicker = function() {
+  score++;
+  highScore = Math.max(score, highScore);
+  updateScore();
+}
+setInterval(scoreTicker, 100);
+
+var prevCollision = false;
+
+var detectCollisions = function() {
+  var collision = false;
+
+  asteroids.each(function() {
+    var cx = this.offsetLeft + settings.r;
+    var cy = this.offsetTop + settings.r;
+
+    var x = cx - mouse.x;
+    var y = cy - mouse.y;
+
+    if ( Math.sqrt(x*x + y*y) < settings.r*2 ) {
+      collision = true;
+    }
+  });
+
+  if ( collision ) {
+    score = 0;
+    if ( prevCollision !== collision) {
+      collisionCount++;
+    }
+  }
+  prevCollision = collision;
+};
+
+d3.timer(detectCollisions);
+  
